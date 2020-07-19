14c9b656b70ed4c70affa5a15a82cb01eec249a0
Ayush Shukla <ayush.shukla@ves.ac.in>
2020-07-20 01:52:31 +0530

    COMMIT2 Added changes n product to fetch country name as weel

--- Server/models/product.model.js
+++ Server/models/product.model.js
@@ -30,7 +30,7 @@ Product.getAll = result => {
 };
 
 Product.findByCategory = (id, result) => {
-    sql.query("SELECT B.ID,B.Name,B.Description,B.Category_ID,B.Company_ID,B.Show,B.Advertise,B.ImageNamelg,B.ImageNamesm,C.Name As CompanyName,C.Country_ID as Country_ID FROM IndiaIndoor_DEV.Brands B LEFT JOIN IndiaIndoor_DEV.Companies C ON B.Company_ID = C.ID WHERE B.Category_ID = ?;",id ,(err, res) => {
+    sql.query("SELECT B.ID,B.Name,B.Description,B.Category_ID,B.Company_ID,B.Show,B.Advertise,B.ImageNamelg,B.ImageNamesm,C.Name As CompanyName,C.Country_ID as Country_ID,CO.Name As CountryName FROM IndiaIndoor_DEV.Brands B LEFT JOIN IndiaIndoor_DEV.Companies C ON B.Company_ID = C.ID LEFT JOIN IndiaIndoor_DEV.Countries CO ON C.Country_ID = CO.ID WHERE B.Category_ID = ?;",id ,(err, res) => {
       if (err) {
         console.log("error: ", err);
         result(null, err);
